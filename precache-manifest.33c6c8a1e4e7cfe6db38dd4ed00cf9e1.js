05d5eb632d7f4514c5a200d9be84918bd65066b9
Theodoros Itzaris <theo.itzaris@circusstreet.com>
2019-11-27 09:14:36 +0000

    Updates

---
+++ precache-manifest.33c6c8a1e4e7cfe6db38dd4ed00cf9e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2432b7eac6fd1ab5c41f2e3c322852",
+    "url": "/react-workspaces-playground/index.html"
+  },
+  {
+    "revision": "5c5182f82175c113fc76",
+    "url": "/react-workspaces-playground/static/css/main.a45bd7a1.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/react-workspaces-playground/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "5c5182f82175c113fc76",
+    "url": "/react-workspaces-playground/static/js/main.7a02f3c4.chunk.js"
+  },
+  {
+    "revision": "d1c96b65410ffd18ff84",
+    "url": "/react-workspaces-playground/static/js/runtime~main.85e163ea.js"
+  },
+  {
+    "revision": "2c813d88489d34f9b9f954e2fac67f5d",
+    "url": "/react-workspaces-playground/static/media/ReactLogo.2c813d88.svg"
+  },
+  {
+    "revision": "42730579f20fe80bb9c071bcadbcdd6a",
+    "url": "/react-workspaces-playground/static/media/YarnCat.42730579.svg"
+  }
+]);
